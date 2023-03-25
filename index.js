8df5e01e4faf4144439b6cb3c8952b60541bb1c1
Elaminebendaas <33202205+Elaminebendaas@users.noreply.github.com>
2023-03-25 18:39:54 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 5000;
+
+let items = [
+    {
+        itemName: "Apple 🍎",
+        price: 2.99,
+        stock: 48
+    },
+    {
+        itemName: "Carrot 🥕",
+        price: 1.99,
+        stock: 98
+    }
+];
+
+app.use(bodyParser.json());
+
+
+app.get('/items', (req, res) => {
+    res.send(items);
+});
+
+
+
+app.post('/items', (req, res) =>{
+    items.push(req.body);
+    res.send("We have received your request and pushed to db")
+});
+
+app.patch('/items/:id', (req, res) => {
+
+
+});
+
+app.delete('/items/:id', (req, res) => {
+    res.send(` ${JSON.stringify(items[req.params.id - 1])} The item shown has been deleted no going back :P`);
+    items.splice(req.params.id - 1, 1);
+});
+
+
+app.listen(port, () => {
+    console.log("server is up and running!");
+});
+
